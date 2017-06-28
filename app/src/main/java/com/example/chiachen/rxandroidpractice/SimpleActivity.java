afb44e25b9f452f950eb2d10bfe0bf012a3e17a8
ChiaChen <s124202468@gmail.com>
2017-06-28 22:56:52 +0800

    Practice-2 mapping

--- app/src/main/java/com/example/chiachen/rxandroidpractice/SimpleActivity.java
+++ app/src/main/java/com/example/chiachen/rxandroidpractice/SimpleActivity.java
@@ -2,9 +2,14 @@ package com.example.chiachen.rxandroidpractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import rx.Observable;
-import rx.Subscriber;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 
 /**
@@ -12,22 +17,29 @@ import rx.Subscriber;
  */
 
 public class SimpleActivity extends AppCompatActivity {
-	Observable.OnSubscribe mObservableAction = new Observable.OnSubscribe<String>() {
-		@Override public void call(Subscriber<? super String> subscriber) {
-			subscriber.onNext(sayMyName()); // 发送事件
-			subscriber.onCompleted(); // 完成事件
-		}
-	};
-
-	private String sayMyName() {
-		return "Hello, I am your friend, Spike!";
-	}
-
+	@BindView(R.id.title_simple) TextView mTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple);
+		ButterKnife.bind(this);
+		rx.Observable<String> obShow = rx.Observable.just(getName());
+		obShow.observeOn(AndroidSchedulers.mainThread()).map(mUpperLetterFunc).subscribe(mToastAction);
 	}
+
+	public String getName(){
+		return "im jason";
+	}
+
+	// string -> upper case
+	private Func1<String, String> mUpperLetterFunc = string -> {
+		return string.toUpperCase(); // 大小字母
+	};
+
+	// Shoe toast
+	private Action1<String> mToastAction = string -> {
+		Toast.makeText(SimpleActivity.this, string, Toast.LENGTH_SHORT).show();
+	};
 }
 
