1205f5fdb599a6e56e39ccbc6131d60665d8f184
ChiaChen <s124202468@gmail.com>
2017-06-28 23:32:30 +0800

    Practice-3 send data separately Note: need chain function

--- app/src/main/java/com/example/chiachen/rxandroidpractice/SimpleActivity.java
+++ app/src/main/java/com/example/chiachen/rxandroidpractice/SimpleActivity.java
@@ -2,8 +2,12 @@ package com.example.chiachen.rxandroidpractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,15 +21,29 @@ import rx.functions.Func1;
  */
 
 public class SimpleActivity extends AppCompatActivity {
+	private static final String TAG = "SimpleActivity";
 	@BindView(R.id.title_simple) TextView mTitle;
+
+	final String mManyWords[] = {"Tommy", "jason", "bear", "jimmy", "weting", "wade"};
+	final ArrayList<String> mManyWordsList = new ArrayList<>(Arrays.asList(mManyWords));
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple);
 		ButterKnife.bind(this);
+		//============
 		rx.Observable<String> obShow = rx.Observable.just(getName());
-		obShow.observeOn(AndroidSchedulers.mainThread()).map(mUpperLetterFunc).subscribe(mToastAction);
+		obShow.observeOn(AndroidSchedulers.mainThread()).
+				map(mUpperLetterFunc).
+				subscribe(mToastAction);
+		//============
+
+		rx.Observable<String> obMap = rx.Observable.from(mManyWords);
+		obMap.observeOn(AndroidSchedulers.mainThread()).
+				map(mUpperLetterFunc).
+				subscribe(mLogAction);
+
 	}
 
 	public String getName(){
@@ -40,6 +58,14 @@ public class SimpleActivity extends AppCompatActivity {
 	// Shoe toast
 	private Action1<String> mToastAction = string -> {
 		Toast.makeText(SimpleActivity.this, string, Toast.LENGTH_SHORT).show();
+	};
+
+	private Action1<String> mChangeTextAction = s -> {
+		mTitle.setText(s);
+	};
+
+	private Action1<String> mLogAction = string -> {
+		Log.e(TAG, "eeeeeee  "+string);
 	};
 }
 
